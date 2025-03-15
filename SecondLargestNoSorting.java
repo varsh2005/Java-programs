670f0c3963c640a2f62d1681ef1d2da3766b731a
varsh2005 <119554047+varsh2005@users.noreply.github.com>
2025-03-15 20:13:37 +0530

    Second largest element no sorting

---
+++ SecondLargestNoSorting.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class SecondLargestNoSorting{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for(int i=0;i<n;i++){
+      arr[i] = sc.nextInt();
+    }
+    int largest = Integer.MIN_VALUE;
+    int secondlargest = Integer.MIN_VALUE;
+    for(int i=0;i<n;i++){
+      if(arr[i]>largest){
+        largest = arr[i];
+      }
+      else if(arr[i]>secondlargest && arr[i]!=largest){
+        secondlargest = arr[i];
+      }
+    }
+    if(secondlargest == Integer.MIN_VALUE){
+      System.out.println("No second largest element found");
+    }
+    else{
+      System.out.println(secondlargest);
+    }
+  }
+}
